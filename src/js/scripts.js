b90441339b1df43ca5c1aa59dd5d6be787ffa1c6
John Diggins <rohnfiggins@gmail.com>
2023-04-10 13:14:16 -0700

    first function works

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,42 @@
-function coinCounter(amount) {
-  if (amount === 0) {
-    return {
+let quarters = 0;
+let dimes = 0;
+let nickels = 0;
+let pennies = 0;
 
+
+const coinCounter = (amt) => {
+  if (amt < 25) {
+    if (amt < 10) {
+      if (amt < 5) {
+        if (amt === 0) {
+          console.log("quarters = " + quarters);
+          console.log("dimes = " + dimes);
+          console.log("nickels = " + nickels);
+          console.log("pennies = " + pennies);
+
+        }
+        else {
+          pennies++
+          return coinCounter(amt -1)
+        }
+      }
+      else {
+        nickels++
+        return coinCounter(amt -5)
+      }
     }
-  };
+    else {
+      dimes++
+      return coinCounter(amt -10);
+    }
+
+  }
+  else {
+    quarters ++
+    return coinCounter(amt - 25);
+  }
 }
+
+
+
+
