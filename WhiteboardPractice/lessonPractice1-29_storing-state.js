7ba4ee80625780120895f1935493cfaaf096317a
John Diggins <rohnfiggins@gmail.com>
2023-04-11 13:56:25 -0700

    lesson snippets

--- WhiteboardPractice/lessonPractice1-29_storing-state.js
+++ WhiteboardPractice/lessonPractice1-29_storing-state.js
@@ -37,6 +37,34 @@ const plantFedAgain = stateControl(greenFood);
 const storeState = () => {
   let currentState = { soil: 0, light: 0, water: 0 }; //small change made to function here.
   return (stateChangeFunction) => {
+    const newState = stateChangeFunction(currentState);                                 
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+// a more reusable way for potentially adding all options
+const storeState = (initialState) => {
+  let currentState = initialState; // We could pass in a n initial state to the object instead of starting with an empty object as well.
+  return (stateChangeFunction) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+//this will NOT WORK. blueFood expects to be given an object as an argument. Could do error handling to prevent this in an app.
+// const blueFood = changeState('soil')(5);
+// const fedPlant = stateControl(blueFood());
+// ERROR: Uncaught TypeError: Cannot read property 'soil' of undefined
+
+
+// less relevant ---------------------------------------------------paused about 2/3 way down, where bookmark is-
+
+// storeState function literal again: 
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction) => {
     const newState = stateChangeFunction(currentState);
     currentState = {...newState};
     return newState;
