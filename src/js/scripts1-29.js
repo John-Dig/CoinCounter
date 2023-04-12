de14548686cff0e0442dfcd15fce592805b4bce2
John Diggins <rohnfiggins@gmail.com>
2023-04-11 21:04:31 -0700

    working on lessons

--- src/js/scripts1-29.js
+++ src/js/scripts1-29.js
@@ -1,3 +1,4 @@
+//the awesomeness function
 const storeState = () => {
   let currentState = {}
   return (stateChangeFunction) => {
@@ -6,5 +7,23 @@ const storeState = () => {
     return newState
   }
 }
-
+// storing the function, closing -currentState-, all in a constant that invokes it
 const stateControl = storeState();
+
+//what's technically inside stateControl
+(stateChangeFunction) => {
+  const newState = stateChangeFunction(currentState);
+  currentState = {...newState};
+  return newState;
+}
+
+//pass in a feeding function without invoking
+const fedPlant = stateControl(blueFood);
+{ soil: 5 }
+//now const newState = blueFood(currentState);
+
+//when blueFood(currentState) is called, it invokes:
+(state) => ({
+  ...state,
+['soil']: (state['soil'] || 0) + 5
+})
