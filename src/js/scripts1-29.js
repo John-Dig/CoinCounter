80bfc6a70284b2347744a1582f1313e9c5516e21
John-Dig <rohnfiggins@gmail.com>
2023-04-11 21:48:34 -0700

    working on lesson

--- src/js/scripts1-29.js
+++ src/js/scripts1-29.js
@@ -1,3 +1,7 @@
+//adding blueFood
+const blueFood = changeState("soil")(5)
+
+
 //the awesomeness function
 const storeState = () => {
   let currentState = {}
@@ -11,19 +15,20 @@ const storeState = () => {
 const stateControl = storeState();
 
 //what's technically inside stateControl
-(stateChangeFunction) => {
-  const newState = stateChangeFunction(currentState);
-  currentState = {...newState};
-  return newState;
-}
+// (stateChangeFunction) => {
+//   const newState = stateChangeFunction(currentState);
+//   currentState = {...newState};
+//   return newState;
+// }
 
 //pass in a feeding function without invoking
 const fedPlant = stateControl(blueFood);
-{ soil: 5 }
+//{ soil: 5 }
 //now const newState = blueFood(currentState);
 
 //when blueFood(currentState) is called, it invokes:
-(state) => ({
-  ...state,
-['soil']: (state['soil'] || 0) + 5
-})
+// (state) => ({
+//   ...state,
+// ['soil']: (state['soil'] || 0) + 5
+// })
+
